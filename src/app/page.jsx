36a44b41d2685f6ec63ce5437a3701a532c5dca1
Chris Banford <chris@bsoft.ch>
2023-07-24 20:18:33 +0200

    Changed Title on home page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -177,12 +177,10 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award winning developer studio based in Denmark.
+            Sunset on the Matterhorn North
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are developer studio working at the intersection of design and
-            technology. It’s a really busy intersection though — a lot of our
-            staff have been involved in hit and runs.
+            This is the Matterhorn Art Studio's launch edition &mdash; by artist Chris Banford. 
           </p>
         </FadeIn>
       </Container>
